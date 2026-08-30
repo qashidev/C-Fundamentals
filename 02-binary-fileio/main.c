835b0b790a0de81ee3c8bb2f8737a38bf847a5a9
Qashi <mqasidhussain.personal@gmail.com>
2026-08-31 00:18:07 +1000

    Update main.c

--- 02-binary-fileio/main.c
+++ 02-binary-fileio/main.c
@@ -2,11 +2,16 @@
 #include <string.h>
 
 int main(void){
-  char x[67]; FILE *fp=fopen("output.bin","wb")
-  fgets(x,sizeof(x),stdin);
+  char x[67]; FILE *fp=fopen("output.bin","wb");
   if (fgets(x,sizeof x,stdin)!=NULL) {
         x[strcspn(x,"\n")]='\0';
     }
-  fwrite(x,sizeof(char),strlen(x)-1,fp); //sizeof(char) -> 1; either one depending on arch
+  fwrite(x,sizeof(char),strlen(x),fp); //sizeof(char) -> 1; either one dependin>
+  fclose(fp);
+
+  fp=fopen("output.bin","rb"); unsigned char z;
+  while (fwrite(&z,sizeof(char),1,fp)>0){
+    printf("%d ",z);
+  }printf("\n");
   fclose(fp);
 }
